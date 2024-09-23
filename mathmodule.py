6d8923d20edc96d53e295aa25376778cdec77ce4
Xaden77 <hridhaykrishna2006@gmail.com>
2024-09-23 14:12:42 +0530

    first commit

---
+++ mathmodule.py
@@ -0,0 +1,18 @@
+import math
+sqr=math.sqrt(16)
+power=math.pow(2,5)
+ceil=math.ceil(6.3)
+floor=math.floor(7.8)
+fact=math.factorial(5)
+modulus=math.fabs(-9)
+sin=math.sin(math.pi/2)
+cos=math.cos(math.pi/2)
+tan=math.tan(math.pi/2)
+log2=math.log2(3)
+log10=math.log2(3)
+euler=math.e
+infinity=math.inf
+degrees=math.degrees(math.pi/2)
+nan=math.nan
+gcd=math.gcd(4,5)
+lcm=math.lcm(4,5)
